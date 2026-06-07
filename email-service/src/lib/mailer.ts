20e27476e217aaa509eb4f9432cbf691d58a4f75
ME <accgit@outlook.com>
2026-06-07 23:11:34 +0600

    deploy fix

--- email-service/src/lib/mailer.ts
+++ email-service/src/lib/mailer.ts
@@ -7,15 +7,15 @@ dotenv.config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 // Create a transporter using SMTP
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: process.env.SMTP_USER,
-//     pass: process.env.SMTP_PASS,
-//   },
-// });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 export async function sendEmail(to: string, subject: string, html: string) {
   // const mailOptions = {
@@ -25,6 +25,13 @@ export async function sendEmail(to: string, subject: string, html: string) {
   //   html,
   // };
   // const info = await transporter.sendMail(mailOptions);
+  transporter.verify((err, success) => {
+    if (err) {
+      logger.error({ error: err }, "SMTP connection failed");
+    } else {
+      logger.info({ success }, "SMTP connection successful");
+    }
+  });
   const { data, error } = await resend.emails.send({
     from: process.env.EMAIL_FROM!,
     to,
