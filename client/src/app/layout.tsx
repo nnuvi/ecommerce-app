833688e61a42308b110eaf58fd2f7bfcdf7ad10a
ME <accgit@outlook.com>
2026-06-07 17:49:31 +0600

    deploy fix

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "./../components/Footer";
-import NavBar from "../../src/components/NavBar";
+import NavBar from "../components/NavBar";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import { ClerkProvider } from "@clerk/nextjs";
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,36 @@
+// import Link from "next/link";
+// import Image from "next/image";
+// import ProductList from "../../src/components/ProductList";
+
+// const Homepage = async ({
+//   searchParams,
+// }: {
+//   searchParams: Promise<{ category: string }>;
+// }) => {
+//   const category = (await searchParams).category;
+//   console.log("Homepage searchParams:", { category });
+//   return (
+//     <div className="">
+//       <div className="relative aspect-3/1 mb-8">
+//         <Image src={"/featured1.png"} alt={"Featured Product"} fill></Image>
+//       </div>
+//       <ProductList category={category} params={"homepage"} />
+//     </div>
+//   );
+// };
+
+// export default Homepage;
+
 import Link from "next/link";
 import Image from "next/image";
-import ProductList from "../../src/components/ProductList";
+import ProductList from "../components/ProductList";
 
 const Homepage = async ({
   searchParams,
 }: {
-  searchParams: Promise<{ category: string }>;
+  searchParams?: { category?: string };
 }) => {
-  const category = (await searchParams).category;
+  const category = searchParams?.category;
   console.log("Homepage searchParams:", { category });
   return (
     <div className="">
