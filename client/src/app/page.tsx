0a72b44d8b124c5d87a7cc7f32738b58e2adfef7
ME <accgit@outlook.com>
2026-08-13 21:46:50 +0600

    fix: preview img issue fixed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,9 +10,23 @@ const Homepage = async ({
 
   return (
     <div className="">
-      <div className="relative aspect-3/1 mb-8">
-        <Image src={"/feat.png"} alt={"Featured Product"} fill></Image>
+      <div className="relative w-full aspect-3/1 mb-8">
+        <Image
+          src={"/feat.png"}
+          alt={"Featured Product"}
+          fill
+          priority
+          sizes="100vw"
+        />
       </div>
+      {/* <div className="relative aspect-1920/800 mb-8">
+        <Image
+          src="/feat.png"
+          alt="Featured Product"
+          fill
+          className="object-cover"
+        />
+      </div> */}
       <ProductList category={category} params={"homepage"} />
     </div>
   );
